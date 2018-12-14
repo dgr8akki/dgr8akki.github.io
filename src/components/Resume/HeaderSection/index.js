f94634971f6440fad9832458eef6a5926518cc16
Aakash Pahuja <pahujaaakash5@gmail.com>
2018-12-14 22:26:46 +0530

    Fix header section

--- src/components/Resume/HeaderSection/index.js
+++ src/components/Resume/HeaderSection/index.js
@@ -2,24 +2,20 @@ import React from 'react';
 import style from './HeaderSection.module.scss';
 
 const HeaderSection = ({ className }) => (
-  <div className={className}>
-    <div id="tx1">
-      <span className="ft2">(+91) 799-592-4034 </span>
-      <span className="ft2">pahujaaakash5@gmail.com</span>
+  <div className={`${className} ${style.headerWrapper}`}>
+    <div className={style.personalInfo}>
+      <div>Hi-TechCity</div>
+      <div>Hyderabad, TS</div>
     </div>
-    <div id="id1_1">
-      <div id="id1_1_1">
-        <p className="p0 ft2">Hi-TechCity</p>
-        <p className="p1 ft3">Hyderabad, TS</p>
-      </div>
-      <div id="id1_1_2">
-        <p className="p0 font-small">
-          <span className="font-head">A</span>
-          {' A K A S H '}
-          <span className="font-head">&nbsp;P</span>
-          {' A H U J A'}
-        </p>
-      </div>
+    <div className={style.name}>
+      <span className={style.heading}>A</span>
+      {' A K A S H'}
+      <span className={style.heading}>P</span>
+      {' A H U J A'}
+    </div>
+    <div className={style.contactInfo}>
+      <div>(+91) 799-592-4034</div>
+      <div>pahujaaakash5@gmail.com</div>
     </div>
   </div>
 );
