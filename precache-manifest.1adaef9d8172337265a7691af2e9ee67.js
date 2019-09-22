0df99b278efeb2b2d5e063ae3fad77ecd4e25d65
Aakash Pahuja <pahujaaakash5@gmail.com>
2019-09-22 18:53:03 +0530

    Updates

---
+++ precache-manifest.1adaef9d8172337265a7691af2e9ee67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336700d49a3acb97abab48754c1c844a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b912bd283a53f27f757",
+    "url": "/static/css/main.41294319.chunk.css"
+  },
+  {
+    "revision": "de9aac681a5b0606d37e",
+    "url": "/static/js/2.4b90e641.chunk.js"
+  },
+  {
+    "revision": "8b912bd283a53f27f757",
+    "url": "/static/js/main.cad7c184.chunk.js"
+  },
+  {
+    "revision": "eb5b6b0e74d20b58bad8",
+    "url": "/static/js/runtime-main.5699fb7e.js"
+  },
+  {
+    "revision": "180ef294b14da426c7fff7f458c7c8ab",
+    "url": "/static/media/AakashPahujaResume.180ef294.pdf"
+  }
+]);
