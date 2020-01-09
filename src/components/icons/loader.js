2b63912cf36a2d2dface1d312a7c863e1af6a738
dgr8akki <pahujaaakash5@gmail.com>
2020-01-10 01:32:40 +0530

    Update icon

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
 const IconLoader = () => (
-  <svg
-    width="100"
-    height="100"
-    viewBox="0 0 100 100"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M47.8 28.3H52.8L64.6 59H59.3L56.15 50.5H44.4L41.25 59H36L47.8 28.3ZM50.25 34.65L46.05 46H54.45L50.25 34.65Z"
-      fill="#64FFDA"
-    />
-    <path d="M50 5L11 27V72L50 95L89 73V28L50 5Z" fill="black" stroke="#64FFDA" />
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <title>Loader Logo</title>
+    <g>
+      <g id="B" transform="translate(11.000000, 5.000000)">
+        <path
+          transform="translate(25,30)"
+          d="M11.8 0.299999H16.8L28.6 31H23.3L20.15 22.5H8.4L5.25 31H7.07805e-08L11.8 0.299999ZM14.25 6.65L10.05 18H18.45L14.25 6.65Z"
+          fill="#64FFDA"
+        />
+      </g>
+      <path
+        stroke="#64FFDA"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+                  L 11, 27
+                  L 11, 72
+                  L 50, 95
+                  L 89, 73
+                  L 89, 28 z"
+      />
+    </g>
   </svg>
 );
 
