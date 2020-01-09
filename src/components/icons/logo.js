902f4e05f120d155da0cecb0c4604edae60c8153
dgr8akki <pahujaaakash5@gmail.com>
2020-01-09 22:35:45 +0530

    Update logo background

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 const IconLogo = () => (
-  <svg width="80" height="92" viewBox="0 0 80 92" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M40 1L1 23V68L40 91L79 69V24L40 1Z" fill="black" stroke="#64FFDA"/>
-    <path d="M38.8 31.3H43.8L55.6 62H50.3L47.15 53.5H35.4L32.25 62H27L38.8 31.3ZM41.25 37.65L37.05 49H45.45L41.25 37.65Z" fill="#64FFDA"/>
+  <svg
+    width="220"
+    height="253"
+    viewBox="0 0 220 253"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M110.058 4.125L4 63.9528V186.328L110.058 248.875L216.117 189.047V66.6722L110.058 4.125Z"
+      fill="#0A192F"
+      stroke="#64FFDA"
+      strokeWidth="7"
+    />
+    <path
+      d="M107.826 85.7122H121.826L154.866 171.672H140.026L131.206 147.872H98.3061L89.4861 171.672H74.7861L107.826 85.7122ZM114.686 103.492L102.926 135.272H126.446L114.686 103.492Z"
+      fill="#64FFDA"
+    />
   </svg>
 );
 
